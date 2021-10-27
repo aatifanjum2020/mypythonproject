eb38baf900ba016f8316f58974f6eda350322c9e
Aatif Anjum <aatifanjum2020@outlook.com>
2021-10-27 14:50:59 +0530

    new python programs

---
+++ import_first_function.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3.8
+
+from first_function import odd_or_even , prime_or_composite
+
+
+x = odd_or_even()
+
+print(x)
+
+
+y = prime_or_composite()
+
+
+print(y)
+
+
+
---
+++ json_parse.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3.8
+
+import os
+
+current_directory = os.getcwd()
+
+print(current_directory)
+
+
+print(dir(os))
